14b7bc31fdcdea4d0cc3c1ea78a57f72aa3f6a40
GersonGMR <56897299+GersonGMR@users.noreply.github.com>
2021-03-12 18:27:21 -0600

    usuarios conectados a base de datos real

--- modelo/dao/conexion.php
+++ modelo/dao/conexion.php
@@ -5,7 +5,7 @@ class conexion {
     private static $host = "localhost";
     private static $user = "root";
     private static $pwd = "";
-    private static $bd = "control_usuarios";
+    private static $bd = "convoy";
 
     public static function conectar() {
         return mysqli_connect(conexion::$host, conexion::$user, conexion::$pwd, conexion::$bd);
